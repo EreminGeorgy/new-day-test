3fb6f51022283c842399e304875bdfca033cc841
eremingeorgy <eremingeorgy@yandex.ru>
2022-05-11 15:37:08 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+A test task to https://www.newday.co.uk/
 
 ## Available Scripts
 
@@ -29,42 +27,26 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+Interview Task:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+Create a WCAG 2.1 AA Compliant Modal
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Put any content you want in the modal,
+ensure it is dismissable or closable,
+include comments to help us understand
+your choices, the modal must prevent 
+interaction with th eunderlying page
+until dismissed or actioned
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Write some tests to confirm that the
+modal works and is compliant
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+Create some styles, remember any styles,
+should also be WCAG 2.1 AA Compliant
 
-## Learn More
+NB: Getting the task done is more important than
+it being complete, don't worry about impelementing 
+the solution in a single file
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Feel free to add libraries on the left,
+the test runner is in a tab on the right
--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+
+import "./styles.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+import { Button, Dialog, Classes } from "@blueprintjs/core";
+
+//-------------------------------------------//
+// Please read the attached comments.md      //
+// file for more information about the task. //
+//-------------------------------------------//
+
+export default function App() {
+  const [isOpen, setOpen] = React.useState(false);
+  const [isConfirmed, setConfirmed] = React.useState(false);
+  const input = React.useRef(null);
+
+  const handleClose = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setOpen(false);
+      setConfirmed(false);
+    },
+    [setOpen, setConfirmed]
+  );
+
+  React.useEffect(() => {
+    if (input.current && !isConfirmed && isOpen) {
+      input.current.focus();
+    }
+  }, [input.current, isConfirmed, isOpen]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>NewDay</h1>
+      <button onClick={() => setOpen(true)}>let' see a modal</button>
+
+      <Dialog
+        title="Here it is!"
+        isOpen={isOpen}
+        onClose={handleClose}
+        aria-labelledby="title"
+        aria-describedby="info"
+        aria-modal="true"
+        usePortal={false}
+      >
+        <section className={Classes.DIALOG_BODY}>
+          <h5 id="title">WCAG 2.1 AA Compliant Modal</h5>
+          <p id="info">
+            <strong>This is an example modal window</strong>
+          </p>
+          <form id="myForm">
+            <fieldset legend="sample form">
+              <div className="field" title="user name input">
+                <label htmlFor="username">User name input</label>
+                <input
+                  name="username"
+                  id="username"
+                  ref={input}
+                  className="bp4-input"
+                  type="text"
+                  placeholder="Name"
+                  aria-label="Type your name"
+                  data-testid="name"
+                />
+              </div>
+              <div className="field" title="Is data correct?">
+                <label htmlFor="checkbox">Is data correct?</label>
+                <input
+                  id="checkbox"
+                  type="checkbox"
+                  placeholder="Name"
+                  onChange={() => {
+                    setConfirmed(!isConfirmed);
+                  }}
+                  aria-label="Is data correct?"
+                  data-testid="checkbox"
+                />
+              </div>
+            </fieldset>
+          </form>
+        </section>
+
+        <footer className={Classes.DIALOG_FOOTER}>
+          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+            <Button
+              title="This button is hooked up to close the dialog."
+              onClick={handleClose}
+              data-testid="close"
+            >
+              Close
+            </Button>
+            <Button
+              type="submit"
+              form="myForm"
+              title={
+                isConfirmed
+                  ? "This button submits data."
+                  : "Please, confirm data."
+              }
+              onClick={handleClose}
+              disabled={!isConfirmed}
+              data-testid="submit"
+            >
+              Ok
+            </Button>
+          </div>
+        </footer>
+      </Dialog>
     </div>
   );
 }
 
-export default App;
